e9ff84312af29da91914fe0299d426f7fbd75e11
xiidot1303 <xii1303@inbox.ru>
2023-12-16 18:49:27 +0500

    upgrade apsscheduler

--- scheduled_job/updater.py
+++ scheduled_job/updater.py
@@ -1,7 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import register_events, DjangoJobStore
 from scheduled_job import mailing, uploading
 
 class jobs:
     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
+    scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
+    register_events(scheduler)
     scheduler.add_job(mailing.send_message, 'interval', minutes=5)
     scheduler.add_job(uploading.update_excel, 'interval', minutes=5)
